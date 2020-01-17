9bd44aa6f899b74e31916cfcad25b66a0e9603a5
vandair correia cabral <larbacriadnav@hotmail.com>
2020-01-17 16:23:43 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorApp2_Classes/Client/Classes/Calculos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2_Classes.Client.Classes
+{
+    public class Calculos
+    {
+
+        public static int Somar(int n1, int n2) => n1 + n2;
+        public static int Subtrair(int n1, int n2) => n1 - n2;
+        public static int Multiplicar(int n1, int n2) => n1 * n2;
+        public static int Dividir(int n1, int n2) => n1 / n2;
+
+    }
+}
---
+++ BlazorApp2_Classes/Shared/Model/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp2_Classes.Shared.Model
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
